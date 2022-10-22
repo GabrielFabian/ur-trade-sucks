12aa0c7df03fad933f6760ac4d0fa219e6277f73
gabe fabian <gabe.fabian@gmail.com>
2022-10-22 17:01:30 -0600

    [UTS] enable s3 bucket

---
+++ lambdas/uts-fetch-players/terraform/s3.tf
@@ -0,0 +1,29 @@
+data "archive_file" "uts-fetch-players" {
+  type = "zip"
+
+  source_dir  = "${path.module}/../build"
+  output_path = "${path.module}/../deploy/uts-fetch-players.zip"
+}
+
+resource "random_pet" "lambda_bucket_name" {
+  prefix = "uts-fetch-players"
+  length = 4
+}
+
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket = random_pet.lambda_bucket_name.id
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_object" "uts-fetch-players-lambda" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  key    = "uts-fetch-players.zip"
+  source = data.archive_file.uts-fetch-players.output_path
+
+  etag = filemd5(data.archive_file.uts-fetch-players.output_path)
+}
